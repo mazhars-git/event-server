b033afd1df4623b8f859847ddc9fee86f2df455a
mazhars-git <mazharulislam611@gmail.com>
2025-07-22 01:07:39 +0600

    update gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,28 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+    // globals: {
+    //   process: "readonly",
+    // },
+  },
   tseslint.configs.recommended,
 ]);
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 
 // parser
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.get("/", (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
-});
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
 
-export default app;
+  res.send(a)
+})
+
+export default app
